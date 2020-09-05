e1634fb5c714a19a2e5ceec0551b01cc804a0d72
Mike <notubw@gmail.com>
2020-09-05 03:07:47 -0500

    Again and

---
+++ againand.cpp
@@ -0,0 +1,10 @@
+//I made this program to see if I could call the main function inside itself.
+#include <iostream>
+using std::cout;
+using std::endl;
+int main()
+{
+cout<<"Again and"<<endl;
+main();
+return 0;
+}
