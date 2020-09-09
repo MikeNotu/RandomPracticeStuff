95d15755dc78a4bc8384d5f71214063a118c4a4f
Mike <notubw@gmail.com>
2020-09-09 01:49:10 -0500

    I helped a Facebook User with his Java Homework

---
+++ AyudaConCalculadora.java
@@ -0,0 +1,24 @@
+//A facebook user needed help with a simple Java calculator program that wasn't working so I fixed his code.
+public class Operaciones{
+	public static void main (String args[]){
+		int operacion = 1;
+		int num_uno = 8;
+		int num_dos = 4;
+		int resultado=0;
+
+		if(operacion==1){
+			resultado = num_uno+num_dos;
+			System.out.println("el resultado de la suma es: "+resultado);
+		}else if(operacion==2){
+			resultado = num_uno-num_dos;
+			System.out.println("el resultado de la resta es: "+resultado);
+		}else if(operacion==3){
+			resultado = num_uno*num_dos;
+			System.out.println("el resultado de la multiplicacion es: "+resultado);
+		}else if(operacion==4){
+			resultado = num_uno/num_dos;
+			System.out.println("el resultado de la division es: "+resultado);
+
+		}
+	}
+}
